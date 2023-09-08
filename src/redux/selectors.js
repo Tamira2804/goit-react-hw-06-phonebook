7661b0eb04131321fd3ee7af55679a12d68642d9
Tamira2804 <tamira2804@gmail.com>
2023-09-08 11:08:29 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,26 +2,13 @@ import { createSelector } from 'reselect';
 
 export const getContactsList = state => state.contacts.list;
 
-export const isContactExists = createSelector(
-  [getContactsList],
-  contactsList => name => {
-    return contactsList.some(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
+export const getFilter = state => state.filter;
+
+export const getFilteredContacts = createSelector(
+  [getContactsList, getFilter],
+  (contactList, filter) => {
+    return contactList.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
     );
   }
 );
-
-const getFilteredContactsSelector = state => {
-  const filter = state.filter.toLowerCase();
-  const contactsList = state.contacts.list;
-  return contactsList.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
-  );
-};
-
-export const getFilteredContacts = createSelector(
-  [getFilteredContactsSelector],
-  filteredContacts => filteredContacts
-);
-
-export const getFilter = state => state.filter;
